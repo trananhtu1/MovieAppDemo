e52fd4f2f0560d1ef7f16454dea1f76585563963
iAnh Tú <108882906+trananhtu1@users.noreply.github.com>
2024-12-04 18:49:23 +0700

    Update firebase.js
    Tránh lộ API Key

--- config/firebase.js
+++ config/firebase.js
@@ -4,13 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Cấu hình Firebase của bạn
 const firebaseConfig = {
-  apiKey: "AIzaSyBdZhPhjI7-9wBNKTNykuM3im4KDV8BdfY",
-  authDomain: "movie-app-login-e6161.firebaseapp.com",
-  projectId: "movie-app-login-e6161",
-  storageBucket: "movie-app-login-e6161.appspot.com",
-  messagingSenderId: "140135156183",
-  appId: "1:140135156183:web:0ffc19800d98a5c123e392",
-  measurementId: "G-BSDDEB182S"
+ 
 };
 
 // Khởi tạo Firebase
